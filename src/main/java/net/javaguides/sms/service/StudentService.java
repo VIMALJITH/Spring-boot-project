726cb0d5e91e7b469e422f1a06b193a14b59ab0e
Vimaljith k <vimaljithk12@gmail.com>
2022-06-02 12:57:10 -0700

    Annotated controller class and added getallStudents in students and implemented that and also added handelr method for getallstudents

--- src/main/java/net/javaguides/sms/service/StudentService.java
+++ src/main/java/net/javaguides/sms/service/StudentService.java
@@ -1,5 +1,13 @@
 package net.javaguides.sms.service;
 
+import java.util.List;
+
+import net.javaguides.sms.entity.Student;
+
 public interface StudentService {
+	
+	List<Student> getAllStudents();
+	
+	
 
 }
